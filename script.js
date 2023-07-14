3c74de3226a2334ac4a3c0aa4079297ec6410db2
Shirley <90633395+Sasley@users.noreply.github.com>
2023-07-13 21:44:26 -0400

    modified css

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var apiKey = "2f7eb335b9e6b805627e7b9635951372"
 var rootUrl = "https://api.openweathermap.org"
-var apiKey2 = "090e4e64cd7d949a4ec9129c4689cd95"
+// var apiKey2 = "090e4e64cd7d949a4ec9129c4689cd95"
 var searchInput = document.querySelector ("#search-input")
 var searchForm = document.querySelector ("#searchform")
 
 
+
 async function fetchCoordinates (searchcity) { 
-    const response = await fetch(`${rootUrl}/data/2.5/weather?q=${searchcity}&appid=${apiKey2}`)
+    const response = await fetch(`${rootUrl}/data/2.5/weather?q=${searchcity}&appid=${apiKey}`)
     const data = await response.json()
     console.log(data)
 }
@@ -21,3 +22,7 @@ function handleFormSubmit (event){
     fetchCoordinates(search)
 }
 searchForm.addEventListener("submit", handleFormSubmit)
+
+// function 5dayForecast(){
+   
+ 
